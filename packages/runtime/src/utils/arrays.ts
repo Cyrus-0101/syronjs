d78a8eaab4e52a5b995129e504ddcd141957ecb6
Cyrus-0101 <cyrus_g@outlook.com>
2024-05-26 20:48:47 +0300

    chore: Still figuring out jsdoc

--- packages/runtime/src/utils/arrays.ts
+++ packages/runtime/src/utils/arrays.ts
@@ -3,8 +3,8 @@ import { type VNode } from './types'
 
 /**
  * @name withoutNulls
- * @param {Array<VNode>} arr
- * @returns {Array<VNode>}
+ * @param arr {Array<VNode>}
+ * @returns Array<VNode>
  * @description withoutNulls(arr: Array<VNode>) - takes an array and returns a new array with all null values removed.
  */
 export const withoutNulls = (arr: Array<VNode>): VNode[] => {
@@ -13,8 +13,8 @@ export const withoutNulls = (arr: Array<VNode>): VNode[] => {
 
 /***
  * @name lipsum
- * @param num {number}
- * @returns {VNode}
+ * @param num number
+ * @returns VNode
  * @description lipsum(num: number) - takes a number and returns a virtual DOM fragment with the number of paragraphs of "Lorem ipsum" text specified by the number passed to the function.
  */
 export const lipsum = (num: number): VNode  => {
@@ -24,9 +24,9 @@ export const lipsum = (num: number): VNode  => {
 
 /**
  * @name MessageComponent
- * @param level {string} 'info' | 'warning' | 'error'
- * @param message {string}
- * @returns {VNode}
+ * @param level string - 'info' | 'warning' | 'error'
+ * @param message string
+ * @returns VNode
  * 
  * @description MessageComponent({ level, message }: { level: 'info' | 'warning' | 'error', message: string }) - takes a level and message and returns a virtual DOM node with the message and level passed to the function.
  */
